67792b96bab715cfaacad64411ef88b75883b281
ShubhamSinghRawat007 <127177695+ShubhamSinghRawat007@users.noreply.github.com>
2024-11-12 14:26:20 +0530

    Some miner changes in heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`} >Hii, I'm <span className='text-[#915eff] ' >Shubham</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className='sm:block hidden' /> interface and web applications.</p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop user friendly,<br className='sm:block hidden' /> Responsive web applications.</p>
         </div>
       </div>
         <ComputersCanvas/>
