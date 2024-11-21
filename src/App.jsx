e24b15f44e8e0d9855b8a66474d53b81f3f2b460
ShubhamSinghRawat007 <127177695+ShubhamSinghRawat007@users.noreply.github.com>
2024-11-21 19:27:03 +0530

    vercle analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import { BrowserRouter } from "react-router-dom";
-
+import { Analytics } from '@vercel/analytics/react';
 import { About, Contact, Experience, Feedbacks, Navbar, Tech, Works, HeroSec } from "./components";
 
 
 function App() {
   return (
     <BrowserRouter>
+
+      <Analytics />
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <HeroSec />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <Feedbacks/>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
         <div className="relative z-0">
-          <Contact/>
-          
+          <Contact />
+
         </div>
       </div>
     </BrowserRouter>
