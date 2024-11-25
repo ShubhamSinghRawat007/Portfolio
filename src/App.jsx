38e1891a8f40c49451b28d788db6f0103336b53d
ShubhamSinghRawat007 <127177695+ShubhamSinghRawat007@users.noreply.github.com>
2024-11-25 19:31:39 +0530

    brevo tracker

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { About, Contact, Experience, Feedbacks, Navbar, Tech, Works, HeroSec } f
 function App() {
   return (
     <BrowserRouter>
-
       <Analytics />
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
@@ -20,7 +19,6 @@ function App() {
         <Feedbacks />
         <div className="relative z-0">
           <Contact />
-
         </div>
       </div>
     </BrowserRouter>
